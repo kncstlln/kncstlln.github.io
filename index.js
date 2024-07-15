4fca76e981a5cbe696115fdb8fc36aba2e78c84a
kncstlln <kanecasti0615@gmail.com>
2024-07-15 16:36:58 +0800

    added clarity

--- index.js
+++ index.js
@@ -4,5 +4,19 @@ new DotLottie({
     autoplay: true,
     loop: true,
     canvas: document.getElementById("canvas"),
-    src: "https://lottie.host/0c759577-6f3e-453a-b95d-ba89ceefde2b/W2cjDRtEuh.json", // or .json file
+    src: "https://lottie.host/0c759577-6f3e-453a-b95d-ba89ceefde2b/W2cjDRtEuh.json",
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const typewriterText = document.getElementById('typewriter-text');
+    const texts = ['Be right back...', 'Updating...'];
+    let index = 0;
+    const animationDuration = 7000; 
+
+    function swapText() {
+        index = (index + 1) % texts.length;
+        typewriterText.textContent = texts[index];
+    }
+
+    setInterval(swapText, animationDuration);
 });
