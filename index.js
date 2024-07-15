9d4536fdd0b64a81ecae3e2d8222a4fbc38fc52e
kncstlln <kanecasti0615@gmail.com>
2024-07-15 14:29:35 +0800

    updating portfolio

---
+++ index.js
@@ -0,0 +1,8 @@
+import { DotLottie } from "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm";
+
+new DotLottie({
+    autoplay: true,
+    loop: true,
+    canvas: document.getElementById("canvas"),
+    src: "https://lottie.host/0c759577-6f3e-453a-b95d-ba89ceefde2b/W2cjDRtEuh.json", // or .json file
+});
